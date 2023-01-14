68f7780affabb0bd2a810bfbae97a25324088625
mxnuchim <manuchimoliver779@gmail.com>
2023-01-14 03:27:20 +0100

    Initial Commit

---
+++ SweaterShopApp/SweaterShopAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SweaterShopAppApp.swift
+//  SweaterShopApp
+//
+//  Created by Manuchim on 14/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct SweaterShopAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
